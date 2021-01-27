c0c3b40413c65bf8b8f3e03bb03ef8d4b30ed906
vinisaveg <viniisaveeg@gmail.com>
2021-01-27 09:06:00 -0300

    add Homepage Content :floppy_disk:

--- src/content/homepage.ts
+++ src/content/homepage.ts
@@ -1,1 +1,44 @@
-export default {};
+export default {
+    hero: {
+        title: 'The Intergalactic lab for milkshakes',
+        subTitle:
+            'Explore, find and create milkshakes with a whole galaxy of flavor',
+    },
+    features: {
+        first: {
+            title: 'Explore',
+            text: 'Lots of milkshakes from people around the world',
+        },
+        second: {
+            title: 'Share',
+            text: 'Share with everyone your delicious milkshake recipe',
+        },
+        third: {
+            title: 'Favorite',
+            text:
+                'Find tasty recipes and save them all in your favorites section',
+        },
+        fourth: {
+            title: 'Custom Icon',
+            text: 'For each recipe, an unique milkshake icon to share ',
+        },
+    },
+    about: {
+        what: {
+            title: 'For each recipe, an unique milkshake icon',
+            text:
+                'Select the best color that fits your awesome milkshake recipe, and as you create your new recipe, this icon will be attatched to it',
+        },
+        how: {
+            title: 'How is it done? Some mystical css clip path?',
+            text:
+                'That’s even more simple. A svg as a milkshake icon will be filled with the colors choosed from the color picker',
+            tip: 'Choose wisely, and your colors will make the icon POP',
+        },
+        why: {
+            title: 'Share your briliant and tasty recipe to the community',
+            text:
+                'Let the world know about your expertise on making the best and most flavored milkshake.',
+        },
+    },
+};
