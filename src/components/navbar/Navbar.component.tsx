0694408a80e8b53a6e1e5660b15fb3c0dddcd442
vinisaveg <viniisaveeg@gmail.com>
2021-02-12 07:52:09 -0300

    Navbar component updated w signed in user info :zap: :wrench:

--- src/components/navbar/Navbar.component.tsx
+++ src/components/navbar/Navbar.component.tsx
@@ -9,8 +9,26 @@ import {
     RegisterButtonWrapper,
 } from './navbar.styles';
 import { Button } from '../../styles/shared/button/button.styles';
+import { useAuth } from '../../hooks/auth/auth';
+import { graphqlClient } from '../../config/graphqlClient';
+import { logoutUserQuery } from '../../graphql/queries/user/logout.query';
+import { LogoutResponse } from '../../types/auth/LogoutResponse.type';
 
 const Navbar: FunctionComponent = () => {
+    const { data, mutate } = useAuth();
+
+    const handleLogout = async () => {
+        const response = await graphqlClient.request<LogoutResponse>(
+            logoutUserQuery
+        );
+
+        if (response.logoutUser) {
+            await mutate();
+        }
+
+        return;
+    };
+
     return (
         <Wrapper>
             <Logo>milkway</Logo>
@@ -26,15 +44,27 @@ const Navbar: FunctionComponent = () => {
 
                 <NavLink href="#about">about</NavLink>
 
-                <Link href="/signin" passHref>
-                    <NavLink>sign in</NavLink>
-                </Link>
+                {data?.authUser ? (
+                    <NavLink onClick={() => handleLogout()}>logout</NavLink>
+                ) : (
+                    <Link href="/signin" passHref>
+                        <NavLink>sign in</NavLink>
+                    </Link>
+                )}
             </NavWrapper>
 
             <RegisterButtonWrapper width="150px" color="primary">
-                <Link href="/register" passHref>
-                    <Button color="primary">Register</Button>
-                </Link>
+                {data?.authUser ? (
+                    <Link href="/me" passHref>
+                        <Button color="primary">
+                            {data.authUser.nickname}
+                        </Button>
+                    </Link>
+                ) : (
+                    <Link href="/register" passHref>
+                        <Button color="primary">Register</Button>
+                    </Link>
+                )}
             </RegisterButtonWrapper>
         </Wrapper>
     );
