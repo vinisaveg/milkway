8726a484d46d54f0671d0071ec083f03a5eb33a5
vinisaveg <viniisaveeg@gmail.com>
2021-02-15 11:55:39 -0300

    Responsive navbar :tada: :rocket:

--- src/components/navbar/Navbar.component.tsx
+++ src/components/navbar/Navbar.component.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import Link from 'next/link';
 
 import {
@@ -7,6 +7,10 @@ import {
     NavWrapper,
     NavLink,
     RegisterButtonWrapper,
+    NavIconWrapper,
+    NavIcon,
+    NavIconSpan,
+    SubWrapper,
 } from './navbar.styles';
 import { Button } from '../../styles/shared/button/button.styles';
 import { useAuth } from '../../hooks/auth/auth';
@@ -15,6 +19,7 @@ import { logoutUserQuery } from '../../graphql/queries/user/logout.query';
 import { LogoutResponse } from '../../types/auth/LogoutResponse.type';
 
 const Navbar: FunctionComponent = () => {
+    const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
     const { data, mutate } = useAuth();
 
     const handleLogout = async () => {
@@ -29,43 +34,55 @@ const Navbar: FunctionComponent = () => {
         return;
     };
 
+    const handleNav = () => {
+        isNavOpen ? setIsNavOpen(false) : setIsNavOpen(true);
+    };
+
     return (
         <Wrapper>
             <Logo>milkway</Logo>
 
-            <NavWrapper>
-                <Link href="/home" passHref>
-                    <NavLink>home</NavLink>
-                </Link>
-
-                <Link href="/explore" passHref>
-                    <NavLink>explore</NavLink>
-                </Link>
-
-                <NavLink href="#about">about</NavLink>
-
-                {data?.authUser ? (
-                    <NavLink onClick={() => handleLogout()}>logout</NavLink>
-                ) : (
-                    <Link href="/signin" passHref>
-                        <NavLink>sign in</NavLink>
+            <SubWrapper isNavOpen={isNavOpen}>
+                <NavWrapper>
+                    <Link href="/home" passHref>
+                        <NavLink>home</NavLink>
                     </Link>
-                )}
-            </NavWrapper>
 
-            <RegisterButtonWrapper width="150px" color="primary">
-                {data?.authUser ? (
-                    <Link href="/me" passHref>
-                        <Button color="primary">
-                            {data.authUser.nickname}
-                        </Button>
+                    <Link href="/explore" passHref>
+                        <NavLink>explore</NavLink>
                     </Link>
-                ) : (
-                    <Link href="/register" passHref>
-                        <Button color="primary">Register</Button>
-                    </Link>
-                )}
-            </RegisterButtonWrapper>
+
+                    <NavLink href="#about">about</NavLink>
+
+                    {data?.authUser ? (
+                        <NavLink onClick={() => handleLogout()}>logout</NavLink>
+                    ) : (
+                        <Link href="/signin" passHref>
+                            <NavLink>sign in</NavLink>
+                        </Link>
+                    )}
+                </NavWrapper>
+
+                <RegisterButtonWrapper width="150px" color="primary">
+                    {data?.authUser ? (
+                        <Link href="/me" passHref>
+                            <Button color="primary">
+                                {data.authUser.nickname}
+                            </Button>
+                        </Link>
+                    ) : (
+                        <Link href="/register" passHref>
+                            <Button color="primary">Register</Button>
+                        </Link>
+                    )}
+                </RegisterButtonWrapper>
+            </SubWrapper>
+
+            <NavIconWrapper onClick={() => handleNav()}>
+                <NavIcon isNavOpen={isNavOpen}>
+                    <NavIconSpan isNavOpen={isNavOpen} />
+                </NavIcon>
+            </NavIconWrapper>
         </Wrapper>
     );
 };
--- src/components/navbar/navbar.styles.ts
+++ src/components/navbar/navbar.styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { ButtonWrapper } from '../../styles/shared/button/button.styles';
 
 export const Wrapper = styled.header`
+    position: relative;
     width: 100%;
     display: flex;
     justify-content: space-between;
@@ -10,17 +11,53 @@ export const Wrapper = styled.header`
     margin-bottom: 80px;
 `;
 
+interface SubWrapperProps {
+    isNavOpen: boolean;
+}
+
+export const SubWrapper = styled.nav<SubWrapperProps>`
+    width: 70%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    z-index: 4;
+    box-sizing: border-box;
+
+    @media only screen and (max-width: 990px) {
+        display: ${(props) => (props.isNavOpen ? 'flex' : 'none')};
+        width: 100%;
+        height: 500px;
+        position: absolute;
+        flex-flow: column;
+        justify-content: center;
+        align-items: flex-start;
+        top: -10px;
+        background-color: rgba(255, 255, 255, 0.95);
+        padding: 0px 30px;
+        border-radius: 8px;
+    }
+
+    @media only screen and (max-width: 520px) {
+        width: 90%;
+        right: 5%;
+        left: 5%;
+    }
+`;
+
 export const NavWrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    z-index: 5;
 
     && a:last-child {
         margin-right: 0;
     }
 
-    @media only screen and (max-width: 950px) {
-        display: none;
+    @media only screen and (max-width: 990px) {
+        flex-flow: column;
+        justify-content: center;
+        align-items: flex-start;
     }
 `;
 
@@ -34,6 +71,12 @@ export const NavLink = styled.a`
     &&:hover {
         opacity: 0.85;
     }
+
+    @media only screen and (max-width: 990px) {
+        margin-right: 0px;
+        margin-bottom: 60px;
+        color: ${(props) => props.theme.colors.text.dark};
+    }
 `;
 
 export const Logo = styled.h1`
@@ -46,8 +89,86 @@ export const Logo = styled.h1`
     }
 `;
 
-export const RegisterButtonWrapper = styled(ButtonWrapper)`
-    @media only screen and (max-width: 950px) {
-        display: none;
+export const RegisterButtonWrapper = styled(ButtonWrapper)``;
+
+export const NavIconWrapper = styled.div`
+    display: none;
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    border: 1px solid rgba(255, 255, 255, 0.15);
+    border-radius: 8px;
+    margin: 0 auto;
+    z-index: 10;
+
+    @media only screen and (max-width: 990px) {
+        display: block;
+    }
+
+    @media only screen and (max-width: 520px) {
+        margin-right: 40px;
+    }
+`;
+
+interface NavIconProps {
+    isNavOpen: boolean;
+}
+
+export const NavIcon = styled.div<NavIconProps>`
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${(props) =>
+        props.isNavOpen
+            ? 'rgba(11, 38, 65, 0.05)'
+            : 'rgba(255, 255, 255, 0.25)'};
+    border-radius: 8px;
+    cursor: pointer;
+    transform: translate(-5px, -5px);
+    transition: all 0.2s ease-in-out;
+    z-index: 11;
+
+    &:hover {
+        transform: translate(0px, 0px);
+    }
+`;
+
+interface NavIconSpanProps {
+    isNavOpen: boolean;
+}
+
+export const NavIconSpan = styled.span<NavIconSpanProps>`
+    position: relative;
+    background-color: ${(props) =>
+        props.isNavOpen ? props.theme.colors.text.dark : '#ffffff'};
+    width: 24px;
+    height: 1px;
+    transition: all 0.2s ease-in-out;
+    z-index: 12;
+
+    &:before {
+        content: '';
+        width: 24px;
+        height: 1px;
+        position: absolute;
+        top: -8px;
+        transition: all 0.2s ease-in-out;
+        background-color: ${(props) =>
+            props.isNavOpen ? props.theme.colors.text.dark : '#ffffff'};
+    }
+
+    &:after {
+        content: '';
+        width: 24px;
+        height: 1px;
+        position: absolute;
+        top: 8px;
+        transition: all 0.2s ease-in-out;
+        background-color: ${(props) =>
+            props.isNavOpen ? props.theme.colors.text.dark : '#ffffff'};
     }
 `;
