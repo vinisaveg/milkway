0ca67193edaedaccb146d7cba7e6968bb50db568
vinisaveg <viniisaveeg@gmail.com>
2021-02-21 10:29:50 -0300

    Create Recipe Form :zap:

--- src/components/createMilkshake/createMilkshake.component.tsx
+++ src/components/createMilkshake/createMilkshake.component.tsx
@@ -1,11 +1,67 @@
 import { FunctionComponent } from 'react';
 
-import { BorderWrapper, Wrapper } from './createMilkshake.styles';
+import {
+    BorderWrapper,
+    Wrapper,
+    FormWrapper,
+    MilkshakeWrapper,
+    Title,
+    Text,
+    Form,
+    ErrorLabel,
+    Input,
+    InputLabel,
+    TextArea,
+} from './createMilkshake.styles';
 
 const CreateMilkshakeForm: FunctionComponent = () => {
     return (
         <BorderWrapper>
-            <Wrapper className="wrapper"></Wrapper>
+            <Wrapper className="wrapper">
+                <FormWrapper>
+                    <Title>Create your awesome recipe</Title>
+
+                    <Text>
+                        All the needed ingredients and a step by step
+                        instruction are essential
+                    </Text>
+
+                    <Form>
+                        <ErrorLabel>Something went bad</ErrorLabel>
+
+                        <InputLabel htmlFor="name">Name</InputLabel>
+
+                        <Input type="text" id="name" />
+
+                        <InputLabel htmlFor="description">
+                            Description
+                        </InputLabel>
+
+                        <Input type="text" id="description" />
+
+                        <InputLabel htmlFor="ingredients">
+                            Ingredients
+                        </InputLabel>
+
+                        <Input type="text" id="ingredients" />
+
+                        <InputLabel htmlFor="instructions">
+                            Instructions
+                        </InputLabel>
+
+                        <TextArea id="instructions" />
+                    </Form>
+                </FormWrapper>
+
+                <MilkshakeWrapper>
+                    <Title>Make it unique...</Title>
+
+                    <Text>
+                        Choose the colors for your milkshake icon, and let your
+                        recipe be unique
+                    </Text>
+                </MilkshakeWrapper>
+            </Wrapper>
         </BorderWrapper>
     );
 };
--- src/components/createMilkshake/createMilkshake.styles.ts
+++ src/components/createMilkshake/createMilkshake.styles.ts
@@ -22,10 +22,11 @@ export const BorderWrapper = styled.div`
 
 export const Wrapper = styled.div`
     width: 100%;
-    height: 350px;
+    min-height: 350px;
+    padding: 50px;
     display: flex;
     justify-content: space-between;
-    align-items: center;
+    align-items: flex-start;
 
     overflow: hidden;
     transform: translate(-20px, -20px);
@@ -34,12 +35,91 @@ export const Wrapper = styled.div`
     background-color: #0e2c49;
     transition: all 0.2s ease-in-out;
 
+    @media only screen and (max-width: 990px) {
+        flex-direction: column;
+    }
+
     @media only screen and (max-width: 520px) {
         flex-direction: column;
         justify-content: flex-start;
         align-items: flex-start;
+        padding: 50px 40px 50px 40px;
 
         transform: translate(0px, 0px);
         height: auto;
     }
 `;
+
+export const Title = styled.h1`
+    color: ${(props) => props.theme.colors.text.light};
+    font-weight: bold;
+    font-size: 32px;
+    margin-bottom: 30px;
+`;
+
+export const Text = styled.p`
+    color: ${(props) => props.theme.colors.text.light};
+    font-size: 20px;
+    margin-bottom: 50px;
+`;
+
+export const FormWrapper = styled.div`
+    width: 45%;
+
+    @media only screen and (max-width: 990px) {
+        width: 100%;
+    }
+`;
+
+export const Form = styled.form``;
+
+export const ErrorLabel = styled.label`
+    width: 100%;
+    min-height: 20px;
+    color: ${(props) => props.theme.colors.red};
+    font-size: 14px;
+    display: block;
+    margin-bottom: 20px;
+`;
+
+export const InputLabel = styled.label`
+    color: ${(props) => props.theme.colors.text.light};
+    margin-bottom: 15px;
+    display: inline-block;
+`;
+
+export const Input = styled.input`
+    color: ${(props) => props.theme.colors.text.dark};
+    width: 100%;
+    border-radius: 8px;
+    padding: 12px 14px;
+    background-color: ${(props) => props.theme.colors.background};
+    margin-bottom: 15px;
+    font-weight: lighter;
+    border: 1px solid transparent;
+    outline: none;
+    transition: border 0.15s ease-in-out;
+
+    box-sizing: border-box;
+
+    &&:focus {
+        border: 1px solid ${(props) => props.theme.colors.primary};
+    }
+
+    &&:nth-last-of-type(1) {
+        margin-bottom: 30px;
+    }
+`;
+
+export const InputTextArea = Input.withComponent('textarea');
+
+export const TextArea = styled(InputTextArea)`
+    width: 100%;
+    max-width: 100%;
+    min-width: 100%;
+    min-height: 300px;
+    max-height: 320px;
+    height: 300px;
+`;
+
+export const MilkshakeWrapper = styled(FormWrapper)``;
